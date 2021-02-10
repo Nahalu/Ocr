00a38667c3fce6299769211adbe47ef2361607b4
Romain Marino <romain.mard@gmail.com>
2021-02-10 11:50:02 +0100

    feat: update swagger

--- Romain.Marino.Ocr.WebApi/Startup.cs
+++ Romain.Marino.Ocr.WebApi/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +13,16 @@ namespace Romain.Marino.Ocr.WebApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +35,9 @@ namespace Romain.Marino.Ocr.WebApi
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
         }
     }
 }
